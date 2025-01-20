b68ddc2e40a4655142161a2f4c7d3d29fdccfd8e
BayanYo <s445006095@uqu.edu.sa>
2025-01-20 20:53:00 +0300

    Update MoviesDB.sql

--- MoviesDB.sql
+++ MoviesDB.sql
@@ -11,6 +11,28 @@ Genre_ID INT NOT NULL,
 FOREIGN KEY (Director_ID) REFERENCES Director(ID) ON DELETE CASCADE,
 FOREIGN KEY (Genre_ID) REFERENCES Genre(ID) ON DELETE CASCADE
 );
+INSERT INTO film (ID, Title, Language, ReleaseDate, Director_ID, Genre_ID) -- Insert
+VALUES
+(01, 'Interstellar', 'English', '2014-10-26', 101, 1),
+(02, 'The Holdovers', 'English', '2023-10-27', 102, 2),
+(03, 'Gone Girl', 'English', '2014-10-3', 103, 3),
+(04, 'Home Alone', 'English', '1990-11-10', 104, 4),
+(05, 'Donnie Darko', 'English', '2001-10-26', 105, 5),
+(06, 'Hacksaw Ridge', 'English', '2016-10-16', 106, 6),
+(07, 'Dune', 'English', '2021-9-3', 107, 1),
+(08, 'Aftersun', 'English', '2022-10-21', 108, 2),
+(09, 'Forgotten', 'English', '2017-11-29', 109, 3),
+(10, 'Sherlock Holmes', 'English', '2009-12-25', 110, 5),
+(11, 'Black Hawk Down', 'English', '2002-1-18', 111, 6);
+DELETE FROM Film -- Delet
+WHERE Title = 'Black Hawk Down';
+UPDATE Film -- Update
+SET Language = 'Korean'
+WHERE Title = 'Forgotten';
+SELECT Film.Title, Directors.fName, Directors.lName -- Inner join 
+FROM Film
+INNER JOIN Directors
+ON Film.Director_ID = Directors.directorID;
 
 CREATE TABLE Actor (
     ID INT PRIMARY KEY,
